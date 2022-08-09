0c283472717917e2d24ca4aae879e3f653028269
HABIB <elmouini.habib@gmail.com>
2022-08-09 12:55:54 +0200

    changement emplacement img+ integration donnée+css accueil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,18 +1,32 @@
 function photographerFactory(data) {
     // Récupérer les autres propriétés
-    const { name, portrait } = data;
+    const {  id, name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/photographers_id_photos${portrait}`;
 
+    //affichage des photographes
     function getUserCardDOM() {
         // Modifier pour avoir quelque chose qui ressemble aux maquettes
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", 'Photo de profil de ${name}');
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const citycountry = document.createElement("span");
+        citycountry.textContent = city.concat(",", country);
+        const slogan = document.createElement("p");
+        slogan.textContent = tagline;
+        const priceDay = document.createElement("div");
+        priceDay.setAttribute("class", "price");
+        priceDay.textContent = price + "€/jour";
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(citycountry);
+        article.appendChild(slogan);
+        article.appendChild(priceDay);
+        
         return (article);
     }
 
