ff27da661af413bcca9f763500f617f87ab4ee13
HABIB <elmouini.habib@gmail.com>
2022-08-25 16:19:32 +0200

    function likescounter ok

---
+++ scripts/factories/totalLikesFactory.js
@@ -0,0 +1,36 @@
+function getTotalLikes(pictures) {
+
+    const totalLikesContainer               = document.querySelector(".totalLikesContainer");
+    const totalLikes                        = document.createElement("input");
+    const heart                             = document.createElement("label");
+
+    totalLikesContainer.style.position      = "fixed";
+    totalLikesContainer.style.bottom        = "0";
+    totalLikesContainer.style.right         = "170px";    
+    totalLikesContainer.style.background    = "#D88876";
+
+    totalLikes.style.fontSize               = "20px";
+    totalLikes.style.height                 = "100px";
+    totalLikes.style.width                  = "100px";
+    totalLikes.classList.add("totalCounter");
+    totalLikes.style.height                 = "50px";
+    totalLikes.style.border                 = "none";
+    totalLikes.style.width                  = "55px"; 
+    totalLikes.style.fontSize               = "24px";
+    totalLikes.style.background             = "#D88876";
+    totalLikes.style.paddingLeft            = "20px";
+
+    heart.innerHTML                         = '<i class="fas fa-heart" title="Likes total"></i>';
+    heart.style.fontSize                    = "24px";
+
+    totalLikesContainer.appendChild(totalLikes);
+    totalLikesContainer.appendChild(heart);
+
+    // Compte le nombre total de likes
+    let totalCounter = document.querySelector(".totalCounter");
+    total = 0;
+    pictures.forEach((picture) => total += Number(picture.likes));
+    totalCounter.value = total ; 
+
+    
+}
